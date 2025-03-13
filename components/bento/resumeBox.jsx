1f6e98a19afe9749a5711dc7848ffe08def41320
DumbWays31 <elena.ferreira@free.fr>
2025-03-13 15:19:32 +0100

    download cv

--- components/bento/resumeBox.jsx
+++ components/bento/resumeBox.jsx
@@ -5,6 +5,17 @@ import BentoBox from "../bentoBox";
 import { InboxArrowDownIcon, EyeIcon } from "@heroicons/react/24/outline";
 
 export const ResumeBox = () => {
+  const cvUrl = "images/CV_Elena_FERREIRA.pdf";
+
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = cvUrl;
+    link.download = "CV_Elena_FERREIRA.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <BentoBox className="flex flex-col justify-center p-5 md:col-span-2" colSpan={2}>
       <div className="mb-4">
@@ -13,8 +24,10 @@ export const ResumeBox = () => {
       </div>
 
       <div className="flex justify-center items-center gap-8">
-        <InboxArrowDownIcon className="size-8 text-gray-500" />
-        <EyeIcon className="size-8 text-gray-500" />
+        <InboxArrowDownIcon className="size-8 text-gray-500 cursor-pointer" onClick={handleDownload} />
+        <a href={cvUrl} target="_blank" rel="noopener noreferrer">
+          <EyeIcon className="size-8 text-gray-500" />
+        </a>
       </div>
     </BentoBox>
   );
