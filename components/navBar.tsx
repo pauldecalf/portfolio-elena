20207abc879ccc3db5768cf352ce0cc01e586f90
DumbWays31 <elena.ferreira@free.fr>
2025-03-24 09:47:57 +0100

    responsive navbar

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -13,7 +13,7 @@ export default function NavBar() {
       {["All", "About", "Work"].map((item) => (
         <div
           key={item}
-          className={`w-32 cursor-pointer text-center border-2 border-transparent rounded-full px-4 py-2 ${
+          className={`w-24 sm:w-32 cursor-pointer text-center border-2 border-transparent rounded-full px-4 py-2 ${
             activeTab === item ? "bg-navbar" : "text-gray-400"
           } transition-all duration-300`}
           onClick={() => setActiveTab(item)}
