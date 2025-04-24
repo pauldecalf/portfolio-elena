82bc0ed627fa3c3d2274e7233f0b2e5da8725fcf
DumbWays31 <elena.ferreira@free.fr>
2025-04-24 14:18:32 +0200

    en par defaut

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
 import { createI18nMiddleware } from "next-international/middleware";
-import { NextRequest } from "next/server";
 
 const I18nMiddleware = createI18nMiddleware({
-  locales: ["fr", "en"],
+  locales: ["en", "fr"],
   defaultLocale: "en",
 });
 
 export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/en", request.url));
+  }
+
   return I18nMiddleware(request);
 }
 
